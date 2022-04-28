d40aa08cb48305638710d48e28dca969233cf8da
Rob Fowler <rfo@mianos.com>
2022-04-28 21:30:46 +1000

    Working publisher. Working topic subcriber for motor control

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
 #include <Arduino.h>
 
-#include <OneWire.h>
+#include <Button2.h>
 #include <DallasTemperature.h>
-#include <TFT_eSPI.h> 
+#include <ESP32Servo.h>
+#include <OneWire.h>
 #include <SPI.h>
+#include <TFT_eSPI.h>
 #include <WiFi.h>
 #include <Wire.h>
-#include <Button2.h>
-#include <ESP32Servo.h> 
 
-#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
-#include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include <PubSubClient.h>
+#include <WiFiManager.h> 
+#include <StringSplitter.h>
 
+ const char *dname = "stillerator";
 
-Servo myservo;  // create servo object to control a servo
-
+Servo myservo; // create servo object to control a servo
 
 #define AA_FONT_SMALL "NotoSansBold15"
 #define AA_FONT_LARGE "NotoSansBold36"
@@ -28,46 +29,59 @@ Servo myservo;  // create servo object to control a servo
 #endif
 
 #ifndef TFT_SLPIN
-#define TFT_SLPIN   0x10
+#define TFT_SLPIN 0x10
 #endif
 
 TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke custom library
 
-#define ADC_EN          14
-#define ADC_PIN         34
-#define BUTTON_1        35
-#define BUTTON_2        0
-
+#define ADC_EN 14
+#define ADC_PIN 34
+#define BUTTON_1 35
+#define BUTTON_2 0
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS GPIO_NUM_33
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+// Setup a oneWire instance to communicate with any OneWire devices (not just
+// Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
-// Pass our oneWire reference to Dallas Temperature. 
+// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
 int servoPin = 2;
-int pos = 0; 
+int pos = 0;
 
-const char* mqtt_server = "mqtt2.mianos.com";
+const char *mqtt_server = "mqtt2.mianos.com";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-
 WiFiManager wifiManager;
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
+void callback(char *topic_str, byte *payload, unsigned int length) {
+  auto topic = String(topic_str);
 
+  Serial.printf("Message arrived, topic '%s'\n", topic_str);
+  
+  auto splitter = StringSplitter(topic, '/', 3);  // new StringSplitter(string_to_split, delimiter, limit)
+  int itemCount = splitter.getItemCount();
+  Serial.println("Item count: " + String(itemCount));
+
+
+  for(int i = 0; i < itemCount; i++){
+    String item = splitter.getItemAtIndex(i);
+    Serial.println("Item @ index " + String(i) + ": " + String(item));
+  }
+  DynamicJsonDocument doc(1024);
+  auto err = deserializeJson(doc, payload, length);
+  if (err) {
+    Serial.printf("deserializeJson() failed: '%s'\n", err.c_str());
+  } else {
+    String output;
+    serializeJson(doc, output);
+    Serial.printf("payload '%s'\n", output.c_str());
+  }
+}
 
 void reconnect() {
   // Loop until we're reconnected
@@ -80,9 +94,17 @@ void reconnect() {
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
-      // ... and resubscribe
-      client.subscribe("inTopic");
+      String status_topic = String(dname) + "/status";
+      client.publish(status_topic.c_str(), "starting");
+     
+      String cmnd_topic = String("cmnd/") + dname + "/#";
+      client.subscribe(cmnd_topic.c_str());
+#if 0
+      String stat_topic = String("stat/") + dname; 
+      String tele_topic = String("tele/") + dname;
+      client.subscribe(stat_topic.c_str());
+      client.subscribe(tele_topic.c_str());
+#endif
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -93,8 +115,7 @@ void reconnect() {
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   Serial.println("Start");
 
@@ -103,27 +124,27 @@ void setup()
   tft.setRotation(1);
   if (!SPIFFS.begin()) {
     Serial.println("SPIFFS initialisation failed!");
-    while (1) 
+    while (1)
       yield(); // Stay here twiddling thumbs waiting
   }
   Serial.println("\r\nSPIFFS available!");
 
   // ESP32 will crash if any of the fonts are missing
   bool font_missing = false;
-  if (SPIFFS.exists("/NotoSansBold15.vlw") == false) 
+  if (SPIFFS.exists("/NotoSansBold15.vlw") == false)
     font_missing = true;
-  if (SPIFFS.exists("/NotoSansBold36.vlw") == false) 
+  if (SPIFFS.exists("/NotoSansBold36.vlw") == false)
     font_missing = true;
 
   if (font_missing) {
     Serial.println("\r\nFont missing in SPIFFS, did you upload it?");
-    while(1) 
+    while (1)
       yield();
   } else {
     Serial.println("\r\nFonts found OK.");
   }
   tft.loadFont(AA_FONT_LARGE); // Must load the font first
- 
+
   wifiManager.autoConnect("portal");
 
   client.setServer(mqtt_server, 1883);
@@ -131,49 +152,48 @@ void setup()
 
   sensors.begin();
 
-	ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
-	myservo.setPeriodHertz(50);    // standard 50 hz servo
-	myservo.attach(servoPin, 1000, 2000); //
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo.setPeriodHertz(50);           // standard 50 hz servo
+  myservo.attach(servoPin, 1000, 2000); //
 }
 
 const long NANTEMP = -100.0;
 const int period = 1000;
-unsigned long last_loop_time;;
+unsigned long last_loop_time;
 float last_value = NANTEMP;
 const int forced_send_period = 30000;
 unsigned long last_temp_send_time;
 
-void loop()
-{
-	unsigned long now = millis();
+void loop() {
+  unsigned long now = millis();
 
-	if (now >= last_loop_time + period) {
-		last_loop_time  = now;
-   
-		sensors.requestTemperatures(); 
-		float tempC = sensors.getTempCByIndex(0);
+  if (now >= last_loop_time + period) {
+    last_loop_time = now;
 
-		if (tempC != DEVICE_DISCONNECTED_C) {
-			float diff = last_value != NANTEMP ? tempC - last_value : 0.0;
-		
-			if (diff != 0.0) {
+    sensors.requestTemperatures();
+    float tempC = sensors.getTempCByIndex(0);
+
+    if (tempC != DEVICE_DISCONNECTED_C) {
+      float diff = last_value != NANTEMP ? tempC - last_value : 0.0;
+
+      if (diff != 0.0) {
         char buff[30];
-  			snprintf(buff, sizeof(buff), "%0.2f %0.2f", tempC, diff);
+        snprintf(buff, sizeof(buff), "%0.2f %0.2f", tempC, diff);
 
-				tft.fillScreen(TFT_BLACK);
-				tft.drawString(buff, 3, 0, 0);
-			}
-			if (!client.connected()) {
-				reconnect();
-			}
-			if (client.connected()) {
+        tft.fillScreen(TFT_BLACK);
+        tft.drawString(buff, 3, 0, 0);
+      }
+      if (!client.connected()) {
+        reconnect();
+      }
+      if (client.connected()) {
         auto forced = now > last_temp_send_time + forced_send_period;
-				if (diff != 0 || !last_temp_send_time || forced) {
+        if (last_temp_send_time == 0 ||diff != 0 || !last_temp_send_time || forced) {
           StaticJsonDocument<200> doc;
-					doc["sensor"] = 0;
+          doc["sensor"] = 0;
           doc["temp"] = tempC;
           doc["diff"] = diff;
 
@@ -182,16 +202,15 @@ void loop()
           }
           String output;
           serializeJson(doc, output);
-          client.publish("stillerator/status/temp", output.c_str());
-					last_temp_send_time = now;
-				}
-			}
-			last_value = tempC;
-		} else {
-			Serial.println("Error: Could not read temperature data");
-		}
-	}
-	client.loop();
+          String tele_topic = String("tele/") + dname + "/temp0";
+          client.publish(tele_topic.c_str(), output.c_str());
+          last_temp_send_time = now;
+        }
+      }
+      last_value = tempC;
+    } else {
+      Serial.println("Error: Could not read temperature data");
+    }
+  }
+  client.loop();
 }
-
-
